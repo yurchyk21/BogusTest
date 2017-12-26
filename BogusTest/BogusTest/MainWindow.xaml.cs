337ede20141c560a7b2cb3a141375402ca957789
Yura <fanya21@gmail.com>
2017-12-26 21:07:05 +0200

    first

---
+++ BogusTest/BogusTest/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Trans;
+
+namespace BogusTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=10.7.0.5;Initial Catalog=Fanya21;User ID=test;Password=123456qwerty";
+            Mdl.ReadOrderData(connectionString);
+            UserTable.ItemsSource = Mdl.users;
+        }
+    }
+}
---
+++ BogusTest/Trans/mdl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trans
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public User(int Id, string FullName, string Role)
+        {
+            this.Id = Id;
+            this.FullName = FullName;
+            this.Role = Role;
+        }
+    }
+    public class Mdl
+    {
+        public static List<User> users = new List<User>();
+        public static void ReadOrderData(string connectionString)
+        {
+            string queryString = "SELECT dbo.Users.Id, dbo.Users.FirstName + ' ' + dbo.Users.Name + ' ' + dbo.Users.LastName AS Name, dbo.Roles.Name AS Role FROM dbo.Roles INNER JOIN dbo.Users ON dbo.Roles.Id = dbo.Users.RoleId";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    IDataRecord curRow = (IDataRecord)reader;
+                    users.Add(new User(int.Parse(curRow[0].ToString()), curRow[1].ToString(), curRow[2].ToString()));
+                }
+                reader.Close();
+            }
+        }
+        public static void InsertOrderData()
+        {
+
+        }
+    }
+}
